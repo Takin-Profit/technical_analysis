08aa60d0bc2fa22d49c26cc474aa670c0e3bc8ed
g5becks <techstar.dev@hotmail.com>
2023-05-15 00:27:19 -0400

    chore(Tests): cleanup rsi tests

--- test/rsi_test.dart
+++ test/rsi_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// ignore_for_file: prefer-correct-identifier-length
+
 import 'package:technical_indicators/technical_indicators.dart';
 import 'package:test/test.dart';
 
@@ -14,13 +16,13 @@ Future<void> main() async {
   group('TA.rsi tests', () {
     test('RSI Result should have correct length', () async {
       final res = TA.rsi(quotes.closePrices);
-      quotes.close();
+      final _ = await quotes.close();
       final result = await res.toList();
       expect(result.length, 502);
     });
     test('Should return the correct number of results without nan', () async {
       final res = TA.rsi(quotes.closePrices);
-      quotes.close();
+      final _ = await quotes.close();
       final resultList = await res.toList();
       final result = resultList.where((q) => !q.value.isNaN).toList();
       expect(result.length, 489);
@@ -28,7 +30,7 @@ Future<void> main() async {
 
     test('Should return the correct calculation values', () async {
       final res = TA.rsi(quotes.closePrices);
-      quotes.close();
+      final _ = await quotes.close();
       final resultList = await res.toList();
       final result = resultList.toList();
 
@@ -51,7 +53,7 @@ Future<void> main() async {
       );
     });
     test('Should throw exception with small lookBack < 2', () async {
-      quotes.close();
+      final _ = await quotes.close();
 
       expect(
         () => TA.rsi(quotes.closePrices, lookBack: 1),
