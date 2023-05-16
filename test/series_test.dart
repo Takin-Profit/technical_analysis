e2249e33b891566ceedb88147502381a1b2626da
g5becks <techstar.dev@hotmail.com>
2023-05-16 14:22:26 -0400

    test(Series): test createSeries
    add tests for createSeries function

--- test/series_test.dart
+++ test/series_test.dart
@@ -4,22 +4,72 @@
  * license that can be found in the LICENSE file.
  */
 
+import 'package:decimal/decimal.dart';
 import 'package:technical_indicators/technical_indicators.dart';
 import 'package:test/test.dart';
 
-import 'data/test_data.dart';
-
 Future<void> main() async {
-  final data = await getDefault();
-  late QuoteSeries quotes;
-  setUp(() => {
-        quotes = createSeries(data).getOrElse((l) => emptySeries),
-      });
   group('Series tests', () {
-    test('Series should have correct length', () async {
-      final _ = await quotes.close();
-      final result = await quotes.toList();
-      expect(result.length, 502);
+    test('createSeries returns a QuoteSeries for valid input', () async {
+      final quotes = [
+        (
+          date: DateTime.now().subtract(Duration(days: 2)),
+          open: Decimal.fromInt(1),
+          high: Decimal.fromInt(2),
+          low: Decimal.fromInt(1),
+          close: Decimal.fromInt(2),
+          volume: Decimal.fromInt(100)
+        ),
+        (
+          date: DateTime.now().subtract(Duration(days: 1)),
+          open: Decimal.fromInt(2),
+          high: Decimal.fromInt(3),
+          low: Decimal.fromInt(2),
+          close: Decimal.fromInt(3),
+          volume: Decimal.fromInt(200)
+        ),
+      ];
+      final result = createSeries(quotes);
+
+      expect(result.isRight(), equals(true));
+      result.fold((l) => null, (r) async {
+        final first = await r.first;
+        expect(first.date, equals(quotes.first.date));
+        expect(first.open, equals(quotes.first.open));
+        expect(first.high, equals(quotes.first.high));
+        expect(first.low, equals(quotes.first.low));
+        expect(first.close, equals(quotes.first.close));
+        expect(first.volume, equals(quotes.first.volume));
+      });
+    });
+
+    test('createSeries returns an error for duplicate dates', () {
+      final date = DateTime.now();
+      final quotes = [
+        (
+          date: date,
+          open: Decimal.fromInt(1),
+          high: Decimal.fromInt(2),
+          low: Decimal.fromInt(1),
+          close: Decimal.fromInt(2),
+          volume: Decimal.fromInt(100)
+        ),
+        (
+          date: date,
+          open: Decimal.fromInt(2),
+          high: Decimal.fromInt(3),
+          low: Decimal.fromInt(2),
+          close: Decimal.fromInt(3),
+          volume: Decimal.fromInt(200)
+        ),
+      ];
+      final result = createSeries(quotes);
+
+      expect(result.isLeft(), equals(true));
+      expect(
+        result.swap().getOrElse((r) => ""),
+        startsWith('Quotes with duplicate dates found ${date.toString()}'),
+      );
     });
   });
 }
