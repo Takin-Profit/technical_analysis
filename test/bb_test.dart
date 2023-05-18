ecdb33c0606ac4ae8fca3e3b782d0a3ffb7d0777
g5becks <techstar.dev@hotmail.com>
2023-05-18 19:36:09 -0400

    test(BB): more bb tests

--- test/bb_test.dart
+++ test/bb_test.dart
@@ -8,7 +8,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// ignore_for_file: prefer-correct-identifier-length
+// ignore_for_file: prefer-correct-identifier-length,double-literal-format
 
 import 'package:collection/collection.dart';
 import 'package:technical_indicators/technical_indicators.dart';
@@ -44,5 +44,23 @@ Future<void> main() async {
       expect(lower.length, 483);
       expect(middle.length, 483);
     });
+    test(
+      'Should return the correct values for upper band calculated',
+      () async {
+        final res = TA.bb(quotes.closes);
+        final _ = await quotes.close();
+        final results = await res.toList();
+        final res249 = results[249];
+        final res501 = results[501];
+
+        expect(res249.upper.toPrecision(4), 259.5642);
+        expect(res249.lower.toPrecision(4), 251.5358);
+        expect(res249.middle.toPrecision(4), 255.5500);
+
+        expect(res501.upper.toPrecision(4), 273.7004);
+        expect(res501.lower.toPrecision(4), 230.0196);
+        expect(res501.middle.toPrecision(4), 251.8600);
+      },
+    );
   });
 }
