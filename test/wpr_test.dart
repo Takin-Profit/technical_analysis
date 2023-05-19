5c16d0c0021ebc56b7c5daaeeb53a6fba3adf447
g5becks <techstar.dev@hotmail.com>
2023-05-19 16:47:38 -0400

    chore(Tests): move test data to google
    move test data expected results out of repository and into google sheets for wpr.

--- test/wpr_test.dart
+++ test/wpr_test.dart
@@ -11,6 +11,11 @@ import 'package:test/test.dart';
 
 import 'data/test_data.dart';
 
+/*
+ * expected results.
+ * https://docs.google.com/spreadsheets/d/10IKwaJybZHl1xNVBM72QHvIhA8ihjy7wUry9riQLHBw/edit?usp=sharing
+ */
+
 Future<void> main() async {
   final data = await getDefault();
   late QuoteSeries quotes;
