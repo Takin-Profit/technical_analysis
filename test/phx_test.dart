1ab1f24d602ad07e0508a5899199ab4a0359bdf6
g5becks <techstar.dev@hotmail.com>
2023-05-24 18:15:22 -0400

    test(Phx): add phx fast tests

--- test/phx_test.dart
+++ test/phx_test.dart
@@ -7,6 +7,7 @@ import 'package:test/test.dart';
 
 import 'data/test_data.dart';
 
+// ignore_for_file: prefer-correct-identifier-length,double-literal-format
 /*
  *
  * expected results.
@@ -36,24 +37,21 @@ Future<void> main() async {
       final _ = await quotes.close();
       final resultList = await res.toList();
       final result = resultList.where((q) => !q.fast.isNaN).toList();
-      expect(result.length, 992);
+      expect(result.length, 686);
     });
 
-    test('fast Should return the correct calculation results', () async {
+    test('fast Should return the correct results', () async {
       final res = TA.phx(quotes);
       final _ = await quotes.close();
       final results = await res.toList();
-      for (var i = 0; i < results.length; i++) {
-        print('$i ${results[i]}\n');
-      }
-      // print(results);
+
       final result0 = results.first;
       final result7 = results[7];
-      final result218 = results[218];
-      final result223 = results[223];
-      final result562 = results[562];
-      final result731 = results[731];
-      final result866 = results[972];
+      final result77 = results[77];
+      final result136 = results[136];
+      final result244 = results[244];
+      final result478 = results[478];
+      final result679 = results[679];
 
       expect(result0.fast.isNaN, true);
       expect(
@@ -61,122 +59,31 @@ Future<void> main() async {
         isNaN,
         reason: 'should be nan',
       );
+      // still warming up data
       expect(
-        result218.fast,
-        80,
-        reason: 'should be 80',
+        result77.fast.toPrecision(8),
+        closeTo(50.36170597, 0.001),
+        reason: 'should be 50.36170597',
       );
       expect(
-        result223.fast,
-        20,
-        reason: 'should be 20',
+        result136.fast.toPrecision(8),
+        43.54124617,
+        reason: 'should be 43.54124617',
       );
       expect(
-        result562.fast,
-        64.15503,
-        reason: 'should be 64.15503',
+        result244.fast.toPrecision(8),
+        66.99396323,
+        reason: 'should be 66.99396323',
       );
       expect(
-        result731.fast,
-        21.756124,
-        reason: 'should be 21.756124',
+        result478.fast.toPrecision(8),
+        0.95802506,
+        reason: 'should be 0.95802506',
       );
       expect(
-        result866.fast,
-        69.90551288,
-        reason: 'should be 69.90551288',
-      );
-    });
-
-    test('slow Should return the correct calculation results', () async {
-      final res = TA.phx(quotes);
-      final _ = await quotes.close();
-      final results = await res.toList();
-
-      final result0 = results.first;
-      final result7 = results[7];
-      final result218 = results[218];
-      final result223 = results[223];
-      final result562 = results[562];
-      final result731 = results[731];
-      final result866 = results[972];
-
-      expect(result0.fast.isNaN, true);
-      expect(
-        result7.slow,
-        isNaN,
-        reason: 'should be nan',
-      );
-      expect(
-        result218.slow,
-        80,
-        reason: 'should be 80',
-      );
-      expect(
-        result223.slow,
-        20,
-        reason: 'should be 20',
-      );
-      expect(
-        result562.slow,
-        64.15503,
-        reason: 'should be 64.15503',
-      );
-      expect(
-        result731.slow,
-        21.756124,
-        reason: 'should be 21.756124',
-      );
-      expect(
-        result866.slow,
-        69.90551288,
-        reason: 'should be 69.90551288',
-      );
-    });
-
-    test('lsma Should return the correct calculation results', () async {
-      final res = TA.phx(quotes);
-      final _ = await quotes.close();
-      final results = await res.toList();
-
-      final result0 = results.first;
-      final result7 = results[7];
-      final result218 = results[218];
-      final result223 = results[223];
-      final result562 = results[562];
-      final result731 = results[731];
-      final result866 = results[972];
-
-      expect(result0.lsma.isNaN, true);
-      expect(
-        result7.lsma,
-        isNaN,
-        reason: 'should be nan',
-      );
-      expect(
-        result218.lsma,
-        80,
-        reason: 'should be 80',
-      );
-      expect(
-        result223.lsma,
-        20,
-        reason: 'should be 20',
-      );
-      expect(
-        result562.lsma,
-        64.15503,
-        reason: 'should be 64.15503',
-      );
-      expect(
-        result731.lsma,
-        21.756124,
-        reason: 'should be 21.756124',
-      );
-      expect(
-        result866.lsma,
-        69.90551288,
-        reason: 'should be 69.90551288',
+        result679.fast.toPrecision(8),
+        51.81038019,
+        reason: 'should be 51.81038019',
       );
     });
   });
