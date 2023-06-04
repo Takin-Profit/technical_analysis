1a9755502cb70fb3600c5e926244820700978e57
g5becks <techstar.dev@hotmail.com>
2023-06-04 00:37:51 -0400

    feat(TA): add tema indicator

---
+++ lib/src/tema.dart
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+import 'ema.dart';
+import 'series.dart';
+import 'types.dart';
+
+Series<PriceData> calcTema(
+  Series<PriceData> series, {
+  int len = 20,
+}) {
+  final tema = getTema(len: len);
+
+  return series.map(
+    (data) => (
+      date: data.date,
+      value: tema(
+        data.value,
+      ),
+    ),
+  );
+}
+
+double Function(double) getTema({int len = 20}) {
+  final ema1 = getEma(len: len);
+  final ema2 = getEma(len: len);
+  final ema3 = getEma(len: len);
+
+  return (double data) {
+    final ema1Val = ema1(data);
+    final ema2Val = ema2(ema1Val);
+    final ema3Val = ema3(ema2Val);
+
+    if (ema1Val.isNaN || ema2Val.isNaN || ema3Val.isNaN) {
+      return double.nan;
+    }
+
+    return (ema1Val * 3) - (ema2Val * 3) + ema3Val;
+  };
+}
