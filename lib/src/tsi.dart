f4d05884e917249d204957f543a9807891b6e00e
g5becks <techstar.dev@hotmail.com>
2023-06-06 15:11:53 -0400

    fix(TA): :art: more lint fixes

--- lib/src/tsi.dart
+++ lib/src/tsi.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// ignore_for_file: avoid-non-null-assertion
 import 'ema.dart';
 import 'series.dart';
 import 'types.dart';
